a945573baddc3115e997903e8bf7291f8c2c5122
Felipe Camarano <felipecamarano@gmail.com>
2025-11-03 22:14:54 -0300

    ajustando espaçamento

--- cpu/functions_table.go
+++ cpu/functions_table.go
@@ -4,22 +4,22 @@ type ExecFn func(cpu *CPU, inst Instruction)
 
 
 var FuncTable = map[InstrType]ExecFn{
-    R_TYPE: execRType, // ADD, SUB, AND...
-    // I_TYPE: execIType, 
+	R_TYPE: execRType, // ADD, SUB, AND...
+	// I_TYPE: execIType, 
 }
 
 
 var rTypeTable = map[uint8]map[uint8]ExecFn{
-    0x0: {
-        0x00: execADD,
-    },
+	0x0: {
+		0x00: execADD,
+	},
 }
 
 
 func execRType(cpu *CPU, inst Instruction) {
 	if inner, ok := rTypeTable[inst.Funct3]; ok {
-        if fn, ok := inner[inst.Funct7]; ok {
-            fn(cpu, inst)
-        }
-    }
+		if fn, ok := inner[inst.Funct7]; ok {
+			fn(cpu, inst)
+		}
+	}
 }
--- main.go
+++ main.go
@@ -11,10 +11,10 @@ func main() {
     raw := uint32(0b0000000_01011_00010_000_00000_0110011)
     inst := cpu.Decode(raw)
 
-	var cpu cpu.CPU
-	cpu.Registers[inst.Rs1] = 10
-	cpu.Registers[inst.Rs2] = 14
-	cpu.Registers[inst.Rd] = 696969
+    var cpu cpu.CPU
+    cpu.Registers[inst.Rs1] = 10
+    cpu.Registers[inst.Rs2] = 14
+    cpu.Registers[inst.Rd] = 696969
 
     fmt.Printf("Opcode:  0x%02X\n", inst.Opcode)
     fmt.Printf("Type:    %v\n", inst.Type)
@@ -25,9 +25,9 @@ func main() {
     fmt.Printf("Funct7:  %08b\n", inst.Funct7)
     fmt.Printf("Imm:     %032b\n", inst.Imm)
 
-	cpu.Execute(inst)
+    cpu.Execute(inst)
 
-	fmt.Printf("CPU Rs1:   %d\n", cpu.Registers[inst.Rs1])
-	fmt.Printf("CPU Rs2:   %d\n", cpu.Registers[inst.Rs2])
-	fmt.Printf("CPU Rd:    %d\n", cpu.Registers[inst.Rd])
+    fmt.Printf("CPU Rs1:   %d\n", cpu.Registers[inst.Rs1])
+    fmt.Printf("CPU Rs2:   %d\n", cpu.Registers[inst.Rs2])
+    fmt.Printf("CPU Rd:    %d\n", cpu.Registers[inst.Rd])
 }
